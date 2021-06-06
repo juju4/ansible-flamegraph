465a2094252006cfb8ed4e1c1df3680355fb48e7
juju4 <juju4@users.noreply.github.com>
2021-06-06 13:53:05 -0400

    add geerlingguy.java as dependency for jni.h

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -10,6 +10,7 @@ rolesdir=$(dirname $0)/..
 fi
 
 #[ ! -d $rolesdir/juju4.redhat-epel ] && git clone https://github.com/juju4/ansible-redhat-epel $rolesdir/juju4.redhat-epel
+[ ! -d $rolesdir/geerlingguy.java ] && git clone https://github.com/juju4/ansible-role-java.git $rolesdir/geerlingguy.java
 ## galaxy naming: kitchen fails to transfer symlink folder
 #[ ! -e $rolesdir/juju4.flamegraph ] && ln -s ansible-flamegraph $rolesdir/juju4.flamegraph
 [ ! -e $rolesdir/juju4.flamegraph ] && cp -R $rolesdir/ansible-flamegraph $rolesdir/juju4.flamegraph
